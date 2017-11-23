bd0c0b83da935b3c24751255654046c52866434d
whitefire0 <rickhallett@live.co.uk>
2017-11-23 14:14:23 +0000

    initial commit

---
+++ jasmine/spec/isPrototypeOfSpec.js
@@ -0,0 +1,19 @@
+describe('Project isPrototypeOf Tests', function() {
+  
+ /*// These two lines are equivalent.
+dog.isPrototypeOf(myDog);  // native function returns true
+isPrototypeOf(dog, myDog); // your function does the same
+
+// These two lines, similarly should return the same thing.
+dog.isPrototypeOf(empty);  // native function returns false
+isPrototypeOf(dog, empty); // your function does the same
+
+// This should work too.
+Object.prototype.isPrototypeOf(myDog);  // native function returns true
+isPrototypeOf(Object.prototype, myDog); // your function does the same
+
+// Also make sure that your function will work for any number of prototype links.
+isPrototypeOf(canine, myDog) // true*/
+  
+
+});
